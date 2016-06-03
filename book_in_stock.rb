ac784d5bd8b045ff25111d8330de3c0ab3d56d23
mwraich <mwraich28@gmail.com>
2016-06-02 22:43:24 -0400

    Set up CSV Reader

--- book_in_stock.rb
+++ book_in_stock.rb
@@ -1,27 +1,23 @@
 class BookInStock
+  attr_reader :isbn
+  attr_accessor :price
+
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
   end
+  # 
+  # def to_s
+  #     "ISBN: #{@isbn}, price: #{@price}"
+  # end
+  #
+  # def price_in_cents
+  #   Integer(price * 100 + 0.5)
+  # end
+  #
+  # def price_in_cents=(cents)
+  #   @price = cents/ 100.0
+  # end
 
-  def to_s
-      "ISBN: #{@isbn}, price: #{@price}"
-  end
-
-  def isbn
-    @isbn
-  end
-
-  def price
-    @price
-  end
-  
 end
-b1 = BookInStock.new("isbn1", 3)
-puts b1
-
-b2 = BookInStock.new("isbn2", 3.14)
-puts b2
-
-b3 = BookInStock.new("isbn3", '5.67')
-puts b3
+# book = BookInStock.new('isbn1', 12.34)
