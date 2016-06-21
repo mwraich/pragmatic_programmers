d05905dbe5a90305dcd9219f848d4463d1064231
mwraich <mwraich28@gmail.com>
2016-06-20 23:05:07 -0400

    Modules and Mixins

---
+++ Modules/Modules.rb
@@ -0,0 +1,27 @@
+module Debug
+  def who_am_i?
+    puts "#{self.class.name}(id: #{self.object_id}) : #{self.name}"
+  end
+end
+
+class Phonograph
+  include Debug
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class EightTrack
+  include Debug
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+ph = Phonograph.new("West End Blues")
+et = EightTrack.new("Happy Dreams")
+
+ph.who_am_i?
+et.who_am_i?
